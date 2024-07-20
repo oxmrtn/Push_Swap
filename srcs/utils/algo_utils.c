c8be2aa5c55dfa34766aa2e2c4c8924b9dce25fe
Martin Trullard <martin@MacBook-Pro-de-Martin.local>
2024-07-20 23:55:26 +0200

    fix

--- srcs/utils/algo_utils.c
+++ srcs/utils/algo_utils.c
@@ -6,46 +6,72 @@
 /*   By: mtrullar <mtrullar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 19:08:58 by mtrullar          #+#    #+#             */
-/*   Updated: 2024/07/17 18:42:24 by mtrullar         ###   ########.fr       */
+/*   Updated: 2024/07/20 23:55:09 by mtrullar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/push_swap.h"
 
+int from_bottom(t_DLIST *stack, int value)
+{
+    t_DLIST *node;
+    int     i;
+
+    i = 0;
+    node = stack;
+    while (node && node->content < value)
+    {
+        node = node->next;
+        i++;
+    }
+    return (i);
+}
+
+int from_top(t_DLIST *stack, int value)
+{
+    t_DLIST *node;
+    int     i;
+
+    i = 0;
+    node = stack;
+    while (node && node->content < value)
+    {
+        node = node->prev;
+        i++;
+    }
+    return (i);
+}
+
 //Find the index of the node we need to insert
 int find_place(t_DLIST *stack, t_DLIST *node)
 {
     int     i;
-    t_DLIST *cur;
-    t_DLIST *cur_n;
-    
-    cur = ft_last_node(stack);
-    cur_n = cur->prev;
+    // t_DLIST *cur;
+    // //t_DLIST *cur_n;
+
+    // cur = ft_last_node(stack);
+    // cur_n = cur->prev;
     i = 0;
-    if (node->content > cur->content && what_min(stack) == stack->content)
-    {
-        i = 0;
-        ft_printf("COND 1\n");
-    }
-    else if (node->content > what_max(stack))
-    {
-        i = find_max(stack);
-        ft_printf("CA PASSSE ICCI%d\n", i);
-    }
-    else if (node->content < what_min(stack))
-    {
-        i = find_min(stack);
-        ft_printf("laaaaa\n");
-    }
+    //CAS PARFAIT
+    if (from_bottom(stack, node->content) >= from_top(stack, node->content))
+        i = from_top(stack, node->content);
     else
-    {
-        while ((cur && cur_n) && (cur->content > node->content || cur_n->content < node->content))
-        {
-            cur = cur_n;
-            cur_n = cur_n->prev;
-            i++;
-        }
-    }
+        i = from_bottom(stack, node->content) - len_stack(stack);
+    // if (node->content > cur->content && what_min(stack) == stack->content)
+    //     i = 0;
+    // else if (node->content > what_max(stack))
+    //     i = find_max(stack);
+    // else if (node->content < what_min(stack))
+    //     i = find_min(stack);
+    // else
+    // {
+    //     while ((cur && cur_n) && (cur->content > node->content || cur_n->content < node->content))
+    //     {
+    //         cur = cur_n;
+    //         cur_n = cur_n->prev;
+    //         i++;
+    //     }
+    // }
     return (i);
 }
 
